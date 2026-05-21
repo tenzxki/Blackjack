39e116ffd6822e3b4ac65f3bb5f6d90afb9d6315
tenzxki <m.t.rathore@outlook.com>
2026-05-21 14:06:45 +0200

    Forbedret kortstokk-generering funkjsonen og lagt til utdelings funksjonen. Skrev om lagkortstokk() til å bruke nøstede for løkker med i og j indekser. Forbedret objekt strukturen slik at hvert kort nå inneholder både farge, navn og poengverdi. Opprettet delUtStartKort() som bruker .pop() for å flytte kort fra den stokkede bunken til spillerens og dealerens arrays.

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ function lagKortstokk() {
     const farger = ["Hjerter", "Ruter", "Spar", "Kløver"];
     const verdier = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
-    for (let farge of farger) {
-        for (let verdi of verdier) {
+    for (let i = 0; i < farger.length; i++) {
+        for (let j = 0; j < verdier.length; j++) {
             
-            let poeng = parseInt(verdi); 
+        let farge = farger[i];
+        let verdi = verdier[j]; 
+            
+        let poeng = parseInt(verdi);
 
-            if (verdi === "J" || verdi === "Q" || verdi === "K") poeng = 10;
-            if (verdi === "A") poeng = 11;
+        if (verdi === "J" || verdi === "Q" || verdi === "K") poeng = 10;
+        if (verdi === "A") poeng = 11;
 
-            kortstokk.push({ farge: farge, navn: verdi, poeng: poeng });
+        kortstokk.push({ farge: farge, navn: verdi, poeng: poeng });
         }
     }
 }
@@ -33,7 +36,18 @@ function stokkKortstokk() {
 }
 
 stokkKortstokk();
-console.log(kortstokk)
+
+function delUtStartKort() {
+    spillerHånd.push(kortstokk.pop());
+    spillerHånd.push(kortstokk.pop());
+
+    dealerHånd.push(kortstokk.pop());
+    dealerHånd.push(kortstokk.pop());
+
+}
+delUtStartKort()
+
+
 
 
     
