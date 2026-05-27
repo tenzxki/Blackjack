4b1d01ec9a83186f46e4fa84ca9116108986c207
tenzxki <m.t.rathore@outlook.com>
2026-05-27 15:44:53 +0200

    Jeg har laget en beregnpoeng logaritme med automatisk håndtering av Ess. Opprettet Oppdaterskjerm funksjon for rendering av kort og poengsummer i DOM. Lagt til style.display på knapper som trengs. Laget en funksjon for når brukeren klikker Nyrunde. Denne går gjennom funksjoner og lister.

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let kortstokk = [];
 let spillerHånd = [];
 let dealerHånd = [];
 
+const dealerKortContainer = document.getElementById("dealer-kort-container");
+const spillerKortContainer = document.getElementById("spiller-kort-container");
+
+const dealerSumTekst = document.getElementById("dealer-sum");
+const spillerSumTekst = document.getElementById("spiller-sum");
+const saldoTekst = document.getElementById("chips");
+const innsatsTekst = document.getElementById("aktiv-innsats");
+
+const btnHit = document.getElementById("btn-hit");
+const btnStand = document.getElementById("btn-stand");
+const btnNyRunde = document.getElementById("btn-ny-runde");
+
+
 function lagKortstokk() {
     const farger = ["Hjerter", "Ruter", "Spar", "Kløver"];
     const verdier = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
@@ -21,7 +34,7 @@ function lagKortstokk() {
         }
     }
 }
-lagKortstokk();
+
 
 function stokkKortstokk() {
     let stokketBunker = [];
@@ -35,7 +48,7 @@ function stokkKortstokk() {
     kortstokk = stokketBunker;
 }
 
-stokkKortstokk();
+
 
 function delUtStartKort() {
     spillerHånd.push(kortstokk.pop());
@@ -45,9 +58,64 @@ function delUtStartKort() {
     dealerHånd.push(kortstokk.pop());
 
 }
-delUtStartKort()
 
 
+function beregnPoeng(hånd) {
+    let sum = 0;
+    let antallEss = 0;
+
+    for (let i = 0; i < hånd.length; i++) {
+        sum += hånd[i].poeng;
+
+        if (hånd[i].navn === "A"){
+            antallEss++;
+        }
+    }
+
+    while (sum > 21 && antallEss > 0) {
+        sum -= 10;
+        antallEss--;
+    }
+    return sum;
+}
 
 
+function OppdaterSkjerm() {
+    spillerKortContainer.innerHTML = "";
+    dealerKortContainer.innerHTML = "";
+
+    for (let i = 0; i < spillerHånd.length; i++) {
+        spillerKortContainer.innerHTML += `<div class = "kort">${spillerHånd[i].farge} ${spillerHånd[i].navn}</div>`;
+    }
+
+    for (let i = 0; i < dealerHånd.length; i++) {
+        dealerKortContainer.innerHTML += `<div class = "kort">${dealerHånd[i].farge} ${dealerHånd[i].navn}</div>`;
+    }
+
+    spillerSumTekst.innerText = beregnPoeng(spillerHånd);
+    dealerSumTekst.innerText = beregnPoeng(dealerHånd);
+}
+
+btnNyRunde.onclick = function() {
+    spillerHånd = [];
+    dealerHånd = [];
+    lagKortstokk();
+    stokkKortstokk();
+    delUtStartKort();
+
+    btnNyRunde.style.display = "none";
+    btnHit.style.display = "inline-block";
+    btnStand.style.display = "inline-block";
+
+    OppdaterSkjerm();
     
+}
+
+btnHit.style.display = "none";
+btnStand.style.display = "none";
+
+
+
+
+
+
